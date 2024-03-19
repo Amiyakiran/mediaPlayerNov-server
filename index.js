f5e6997045b2505e42de176a143e381331983156
amiyakiran <amiyakiran@gmail.com>
2024-03-19 11:17:42 +0530

    final

---
+++ index.js
@@ -0,0 +1,28 @@
+//import json-server library
+const jsonServer = require('json-server')
+
+//create server using create function
+const mediaPlayerServer = jsonServer.create()
+
+//create path for db.json file
+const router = jsonServer.router('db.json')
+
+//create middleware to convert json to js
+const middleware = jsonServer.defaults()
+
+
+//connect
+mediaPlayerServer.use(middleware)
+mediaPlayerServer.use(router)
+
+
+//setup port for the server
+const port = 4000 || process.env.PORT
+
+
+//run the server - 
+
+
+mediaPlayerServer.listen(port,()=>{
+    console.log('MediaPlayer server running successfully');
+})
